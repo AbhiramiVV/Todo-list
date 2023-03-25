307687ff90650fad7ed2fefc84426fbcd03f48be
AbhiramiVV <abhiramivv77@gmail.com>
2023-03-25 10:08:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useRef, useEffect } from "react";
+import { IoMdDoneAll } from "react-icons/io";
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+import "./App.css";
+function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+ const [editId,setEditID] = useState(0)
 
-function App() {
+  const inputRef = useRef("null");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  
+  const addTodo = () => {
+    if(todo.trim() !== ''){
+      setTodos([...todos, {list : todo,id : Date.now(),status :false}]);
+      setTodo("");
+    }
+    if(editId){
+      const editTodo=todos.find((todo)=>todo.id === editId)
+      const updateTodo = todos.map((to)=>to.id === editTodo.id
+      ?(to = {id : to.id,list:todo})
+      :(to ={id :to.id,list:to.list}))
+      setTodos(updateTodo)
+      setEditID(0)
+      setTodo('')
+    }
+    
+  };
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const onDelete = (id) =>{
+   setTodos( todos.filter((to) => to.id !== id))
+
+  }
+
+  const onComplete = (id) =>{
+    let complete=todos.map((list)=>{
+      if(list.id === id){
+        return ({...list,status:!list.status})
+      }
+      return list
+
+    })
+    setTodos(complete)
+
+  }
+
+  const onEdit = (id) =>{
+   
+      const editTodo = todos.find((to)=>to.id === id)
+    
+      setTodo(editTodo.list)
+      setEditID(editTodo.id)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>TODO LIST</h2>
+      <form className="form-group" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          ref={inputRef}
+          placeholder="Enter Your Todo"
+          className="form-control"
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>{editId ? 'EDIT' : 'ADD'}</button>
+      </form>
+      <div>
+        <ul className="list">
+          {todos.map((to) => (
+            <li className="list-items">
+              <div className="list-item-list" id= {to.status?'list-item' :''}> {to.list}</div>
+
+              <span>
+                <IoMdDoneAll
+                  className="list-item-icons"
+                  id="complete"
+                  title="Complete"
+                  onClick={()=>onComplete(to.id)}
+                />
+                <FiEdit className="list-item-icons" id="edit" title="Edit"  onClick={()=>onEdit(to.id)}/>
+                <MdDelete
+                  className="list-item-icons"
+                  id="delete"
+                  title="Delete"
+                  onClick={()=>onDelete(to.id)}
+                />
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Todo;
